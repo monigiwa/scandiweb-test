4b114afdd3cb96b0fa19f21ce50b333cfe2d6cd6
Giwa <giwatitilope899@gmail.com>
2023-04-01 15:10:26 +0100

    updated

--- pages/delete.php
+++ pages/delete.php
@@ -2,41 +2,49 @@
 
 require_once('config.php');
 require_once('dbh.php');
+require_once('config.php');
 
-// Create a new database connection
+class ProductDeletionController {
+    private $database;
 
-try {
-    $databaseConnection = new DatabaseConnection();
-} catch (PDOException $e) {
-    // Display an error message if the database connection fails
-    echo "Unable to create database connection: " . $e->getMessage();
-    exit();
-}
-
-// Create a new Database object using the database connection
-$database = new Database($databaseConnection);
-
-// Check if the form was submitted
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['delete_checkbox'])) {
-    try {
-        // Get the IDs of the products to be deleted
-        $ids = $_POST['delete_checkbox'];
-
-        // Delete the selected products from the database
-        if ($database->deleteProductBtn($ids)) {
-            // If the products were deleted successfully, redirect to the product list page
-            header('Location: ./product_list.php');
-            exit();
-        } else {
-            // If there was an error deleting the products, display an error message
-            echo "Error deleting products from database.";
+    public function __construct() {
+        try {
+            $databaseConnection = new DatabaseConnection();
+            $this->database = new Database($databaseConnection);
+        } catch (PDOException $e) {
+            // Display an error message if the database connection fails
+            echo "Unable to create database connection: " . $e->getMessage();
             exit();
         }
-    } catch (Exception $e) {
-        // Display an error message if there was an error deleting the products
-        echo "Error deleting products from database: " . $e->getMessage();
-        exit();
+    }
+
+    public function deleteProducts() {
+        // Check if the form was submitted
+        if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['delete_checkbox'])) {
+            try {
+                // Get the IDs of the products to be deleted
+                $ids = $_POST['delete_checkbox'];
+
+                // Delete the selected products from the database
+                if ($this->database->deleteProductBtn($ids)) {
+                    // If the products were deleted successfully, redirect to the product list page
+                    header('Location: ./product_list.php');
+                    exit();
+                } else {
+                    // If there was an error deleting the products, display an error message
+                    echo "Error deleting products from database.";
+                    exit();
+                }
+            } catch (Exception $e) {
+                // Display an error message if there was an error deleting the products
+                echo "Error deleting products from database: " . $e->getMessage();
+                exit();
+            }
+        }
     }
 }
+
+$productDeletionController = new ProductDeletionController();
+$productDeletionController->deleteProducts();
 
 ?>
